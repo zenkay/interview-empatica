426b83f429fc320b37f56c5d58c10bd51faefe80
Andrea Mostosi <andrea.mostosi@zenkay.net>
2017-05-26 15:27:41 +0200

    Backend

---
+++ backend/app/Http/Controllers/DownloadController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Download;
+use Illuminate\Http\Request;
+
+class DownloadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $longitude = $request->get('longitude');
+        $latitude = $request->get('latitude');
+        $app_id = $request->get('app_id');
+        $downloaded_at = $request->get('downloaded_at');
+        $user_id = $request->get('user_id');
+
+        $download = new Download();
+        $download->longitude = $longitude;
+        $download->latitude = $latitude;
+        $download->app_id = $app_id;
+        $download->downloaded_at = $downloaded_at;
+        $download->user_id = $user_id;
+
+        $download->save();
+        return array("status" => 200, "message" => "OK");
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Download  $download
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Download $download)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Download  $download
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Download $download)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Download  $download
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Download $download)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Download  $download
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Download $download)
+    {
+        //
+    }
+}
---
+++ backend/database/migrations/2017_05_20_130946_create_downloads_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDownloadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('downloads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('longitude');
+            $table->string('latitude');
+            $table->string('app_id');
+            $table->dateTime('downloaded_at');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('downloads');
+    }
+}
